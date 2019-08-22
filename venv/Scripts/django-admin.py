cc1b48deb3ce86a1717f93fd329f122e3bd77267
Sharon Malio <kaninimalio@gmail.com>
2019-08-22 09:40:32 +0300

    added django and configured the database settings

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\sharon.malio\PycharmProjects\genderApp\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
